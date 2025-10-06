b9f07c3c51e00ee0d93a43334913dbb37e2d61c0
Matthew Quintero <164263560+Mattquin45@users.noreply.github.com>
2025-10-06 15:52:12 -0400

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -14,8 +14,8 @@ function sendEmail({ email, weekData }) {
         const transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
-                user: "noreplycalendarreminder@gmail.com",
-                pass: "isfm woly ufog ryst"
+                user: "Your Email",
+                pass: "Your Email-pass"
             },
         });
         //scheduler cron
